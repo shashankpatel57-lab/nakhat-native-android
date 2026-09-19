f53e5a83d85d7a879a12dc69914831201ec6feec
shashankpatel57-lab <shashankpatel57@gmail.com>
2026-09-19 18:58:17 +0530

    Bump SCANTANTRA to v1.2

--- scanner-ultra/app/build.gradle.kts
+++ scanner-ultra/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "app.lumascan.ultra"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
     buildFeatures { compose = true }
     compileOptions {
